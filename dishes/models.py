89bf7ec465865b7f7cadd2b68f8751c1b2e5f598
Okarin <steinsgate95@mail.ru>
2022-03-13 11:55:45 +0600

    добавил шаблоны списка категорий, списка товаров и детального просмотра товара, сделал пути, вьюшки

--- dishes/models.py
+++ dishes/models.py
@@ -1,3 +1,47 @@
 from django.db import models
+from django.shortcuts import reverse
+from django.template.defaultfilters import slugify
 
-# Create your models here.
+
+class Category(models.Model):
+	slug = models.SlugField(null=True, blank=True, unique=True, db_index=True)
+	name = models.CharField(max_length=30, verbose_name="Название")
+	image = models.ImageField(upload_to="category_image/", null=True, blank=True, verbose_name="Изображение")
+
+	def __str__(self):
+		return self.name
+
+	def get_absolute_url(self):
+		return reverse('category_page', kwargs = {'slug': self.slug})
+
+	def save(self, *args, **kwargs):
+		self.slug = slugify(self.name)
+		return super().save(*args, **kwargs)
+
+
+	class Meta:
+		verbose_name = "Категория"
+		verbose_name_plural = "Категории"
+
+
+class Product(models.Model):
+	slug = models.SlugField(unique=True, db_index=True, null=True, blank=True)
+	title = models.CharField(max_length=50, verbose_name="Название")
+	price = models.DecimalField(max_digits=6, decimal_places=2, verbose_name="Цена")
+	category = models.ForeignKey(Category, on_delete=models.CASCADE, null=True, blank=True)
+	image = models.ImageField(upload_to="product_images/", null=True, blank=True)
+
+	def __str__(self):
+		return self.title
+
+	def get_absolute_url(self):
+		return reverse('product_detail', kwargs = {'slug': self.slug})
+
+	def save(self, *args, **kwargs):
+		self.slug = slugify(self.title)
+		return super().save(*args, **kwargs)
+
+
+	class Meta:
+		verbose_name = "Блюдо"
+		verbose_name_plural = "Блюда"
--- dishes/views.py
+++ dishes/views.py
@@ -1,5 +1,51 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
+from .models import Category, Product
 
 
 def index(request):
+	"""
+	Главная страница
+	""" 
 	return render(request, "dishes/index.html")
+
+
+def categories_page_view(request):
+	""" 
+	Страница со всеми категориями
+	"""
+	categories = Category.objects.all()
+
+	context = {
+		'categories': categories
+	}
+
+	return render(request, "dishes/categories.html", context)
+
+
+def category_product_page_view(request, slug):
+	""" 
+	Страница с продуктами определённой категории
+	"""
+	category = get_object_or_404(Category, slug=slug)
+	products = Category.objects.get(slug=slug).product_set.all()
+
+	context = {
+		'category': category,
+		'products':products,
+	}
+
+	return render(request, "dishes/category_page.html", context)
+
+def product_detail_page_view(request, category_slug, product_slug):
+	""" 
+	Страница детального просмотра продукта
+	"""
+	category = get_object_or_404(Category, slug=category_slug)
+	product = get_object_or_404(Product, slug=product_slug)
+
+	context = {
+		'product': product,
+		'category': category,
+	}
+
+	return render(request, "dishes/product_detail.html", context)
