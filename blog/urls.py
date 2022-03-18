5650d69ef2e30ccbac9fad0af788c5a12b361ebe
Nurjigit <Sagendikov45@gmail.com>
2022-03-18 23:11:37 +0600

    Сделал главный шаблон и our_menu.html

--- blog/urls.py
+++ blog/urls.py
@@ -23,4 +23,5 @@ from django.conf.urls.static import static
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', views.index, name = "index"),
+    path('our_menu', views.our_menu, name = "our_menu")
 ]
--- posts/views.py
+++ posts/views.py
@@ -5,5 +5,9 @@ from django.shortcuts import render
 def index(request):
 	return render (request, "posts/index.html")
 
+def our_menu(request):
+	return render (request, "posts/our_menu.html")
+
+
 
 
