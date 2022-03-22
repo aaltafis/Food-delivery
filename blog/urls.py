1dc277cef87e28719696bf41d3042b7ceb09f058
Nurjigit <Sagendikov45@gmail.com>
2022-03-22 22:22:05 +0600

    адаптив на главный шаблон

--- blog/urls.py
+++ blog/urls.py
@@ -31,7 +31,7 @@ urlpatterns = [
     path('soups', views.soups, name="soups"),
     path('sauces', views.sauces, name="sauces"),
     path('beverages', views.beverages, name="beverages"),
-    
+
+] + static(settings.STATIC_URL, document_root = settings.STATIC_ROOT) + static(settings.MEDIA_URL, document_root = settings.MEDIA_ROOT)
 
 
-]+ static(settings.STATIC_URL, document_root = settings.STATIC_ROOT) + static(settings.MEDIA_URL, document_root = settings.MEDIA_ROOT)
