2801c61507ccedca49fd4d88603237770f6c2ca7
Okarin <steinsgate95@mail.ru>
2022-03-23 13:29:53 +0600

    добавил контент, стили, адаптивность к главной странице

--- dishes/views.py
+++ dishes/views.py
@@ -4,58 +4,47 @@ from django.db.models import Q
 
 
 def index(request):
-	"""
-	Главная страница
-	""" 
+    """ Главная страница """
 
-	categories = Category.objects.all()
-
-	context = {
-		'categories': categories
-	}
-
-	return render(request, "dishes/index.html", context)
+    return render(request, "dishes/index.html")
 
 
 def categories_page_view(request):
-	""" 
-	Страница со всеми категориями
-	"""
-	categories = Category.objects.all()
+    """ Страница со всеми категориями """
 
-	context = {
-		'categories': categories
-	}
+    categories = Category.objects.all()
 
-	return render(request, "dishes/categories.html", context)
+    context = {
+        'categories': categories
+    }
+
+    return render(request, "dishes/categories.html", context)
 
 
 def category_product_page_view(request, slug):
-	""" 
-	Страница с продуктами определённой категории
-	"""
-	category = get_object_or_404(Category, slug=slug)
-	products = Category.objects.get(slug=slug).product_set.all()
+    """ Страница с продуктами определённой категории """
 
-	context = {
-		'category': category,
-		'products':products,
-	}
+    category = get_object_or_404(Category, slug=slug)
+    products = Category.objects.get(slug=slug).product_set.all()
 
-	return render(request, "dishes/products.html", context)
+    context = {
+        'category': category,
+        'products': products,
+    }
+
+    return render(request, "dishes/products.html", context)
+
 
 def product_detail_page_view(request, category_slug, product_slug):
-	""" 
-	Страница детального просмотра продукта
-	"""
-	category = get_object_or_404(Category, slug=category_slug)
-	product = get_object_or_404(Product, slug=product_slug)
-	similar_products = Category.objects.get(slug=category_slug).product_set.filter(~Q(slug=product_slug))
+    """ Страница детального просмотра продукта """
+    category = get_object_or_404(Category, slug=category_slug)
+    product = get_object_or_404(Product, slug=product_slug)
+    similar_products = Category.objects.get(slug=category_slug).product_set.filter(~Q(slug=product_slug))
 
-	context = {
-		'product': product,
-		'category': category,
-		'similar_products': similar_products,
-	}
+    context = {
+        'product': product,
+        'category': category,
+        'similar_products': similar_products,
+    }
 
-	return render(request, "dishes/product_detail.html", context)
+    return render(request, "dishes/product_detail.html", context)
